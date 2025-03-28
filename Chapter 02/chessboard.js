802709a521fe929fbca88bf242b63afed08963b8
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-03-27 18:28:16 -0700

    complete exercises for chapter 2

--- Chapter 02/chessboard.js
+++ Chapter 02/chessboard.js
@@ -1,7 +1,7 @@
-let chessboard = [];
-
-for (let pattern = 0; pattern < 9; pattern++) {
-    return ` #`;
+for (let pattern = 0; pattern < 8; pattern++) {
+    if (pattern % 2 === 0) {
+        console.log(' # # # #\n');
+    } else {
+        console.log('# # # # \n');
+    }
 }
-
-console.log(pattern);
--- Chapter 02/fizzbuzz.js
+++ Chapter 02/fizzbuzz.js
@@ -1,4 +1,4 @@
-for (let fizzBuzz = 1; fizzBuzz <= 100; fizzBuzz = fizzBuzz + 1) {
+for (let fizzBuzz = 1; fizzBuzz < 101; fizzBuzz++) {
     if (fizzBuzz % 3 === 0 && fizzBuzz % 5 !== 0) {
         console.log('Fizz');
     } else if (fizzBuzz % 5 === 0 && fizzBuzz % 3 !== 0) {
